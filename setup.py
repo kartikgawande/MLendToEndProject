b4d53918c68085bab8bf77117b33d70673832071
kartikgawande <kartikgawandeonline@gmail.com>
2024-07-08 13:44:15 +0530

    package is now installed in editable mode along with dependencies

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(path:str)->List[str]:
+    requirement_list = []
+    with open(path) as file_obj:
+        requirement_list=file_obj.readlines()
+        requirement_list = [req.replace('\n',"") for req in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
+setup(name='mle2e',
+      version='0.0.2',
+      author='Kartik',
+      author_email='kartikgawandeonline@gmail.com',
+      packages=find_packages(),
+      install_requires=get_requirements('requirements.txt')
+    )
+
+    
